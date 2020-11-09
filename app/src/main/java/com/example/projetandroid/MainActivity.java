6a5a20095060b59642e5d9471c589321ef339c33
Guindo Mouctar Ousseini <mouctar-ousseini.guindo@etu.univ-orleans.fr>
2020-11-09 17:00:48 +0100

    modification de la label password

--- app/src/main/java/com/example/projetandroid/MainActivity.java
+++ app/src/main/java/com/example/projetandroid/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lesCategories = new ArrayList<>();
-        // getCategories();
+        getCategories();
 
         //updateTest();
 
